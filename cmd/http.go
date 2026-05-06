1a770e241675f08d46f5134148dcbe9087fd219e
yogarp <yogarizky51@gmail.com>
2026-05-06 10:44:07 +0700

    first init wallet service

---
+++ cmd/http.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"ewallet-wallet/helpers"
+	"ewallet-wallet/internal/api"
+	"ewallet-wallet/internal/interfaces"
+	"ewallet-wallet/internal/repository"
+	"ewallet-wallet/internal/services"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ServeHTTP() {
+	d := dependencyInject()
+
+	r := gin.Default()
+
+	r.GET("/health", d.HealthcheckAPI.HealthcheckHandlerHTTP)
+
+	walletV1 := r.Group("/wallet/v1")
+	walletV1.POST("/", d.WalletAPI.Create)
+
+	err := r.Run(":" + helpers.GetEnv("PORT", ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Dependency struct {
+	HealthcheckAPI interfaces.IHealthcheckHandler
+	WalletAPI      interfaces.IWalletHandler
+}
+
+func dependencyInject() Dependency {
+	healthcheckSvc := &services.Healthcheck{}
+	healthcheckAPI := &api.Healthcheck{
+		HealthcheckServices: healthcheckSvc,
+	}
+
+	walletRepo := &repository.WalletRepo{
+		DB: helpers.DB,
+	}
+
+	walletService := &services.WalletService{
+		WalletRepo: walletRepo,
+	}
+
+	walletApi := &api.WalletHandler{
+		WalletService: walletService,
+	}
+
+	return Dependency{
+		HealthcheckAPI: healthcheckAPI,
+		WalletAPI:      walletApi,
+	}
+
+}
